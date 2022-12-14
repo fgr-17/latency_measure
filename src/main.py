728c0461da8df9ea25857c4dcb57f6256491d6f2
Federico Roux <rouxfederico@gmail.com>
2022-12-14 19:35:44 -0300

    new measurments

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ base_dir = '/workspace'
 app_path = f'{base_dir}/src/main.py'
 audiofiles_path = f'{base_dir}/audiofiles'
 loopback_path = f'{audiofiles_path}/loopback/120522'
+loopback_path_141222 = f'{audiofiles_path}/loopback/141222'
 linein_lq_path = f'{audiofiles_path}/linein-lq'
 
 loopback_signal = f'{loopback_path}/signal.wav'
@@ -15,6 +16,14 @@ loopback_responses=(
     f'{loopback_path}/loopback3.wav',
     f'{loopback_path}/loopback4.wav',
     f'{loopback_path}/loopback5.wav'
+)
+
+loopback_responses_141222=(
+    f'{loopback_path_141222}/loopback141222_1900_1.wav',
+    f'{loopback_path_141222}/loopback141222_1900_2.wav',
+    f'{loopback_path_141222}/loopback141222_1900_3.wav',
+    f'{loopback_path_141222}/loopback141222_1900_4.wav',
+    f'{loopback_path_141222}/loopback141222_1900_5.wav'
 )
 
 loopback_compensated=(
@@ -39,6 +48,22 @@ linein_lq_2b=(
     f'{linein_lq_path}/linein-lq-2buf-3.wav',
     f'{linein_lq_path}/linein-lq-2buf-4.wav',
     f'{linein_lq_path}/linein-lq-2buf-5.wav'
+)
+
+linein_lq_21327=(
+    f'{linein_lq_path}/lin-lq-2.13.27-base-1.wav',
+    f'{linein_lq_path}/lin-lq-2.13.27-base-2.wav',
+    f'{linein_lq_path}/lin-lq-2.13.27-base-3.wav',
+    f'{linein_lq_path}/lin-lq-2.13.27-base-4.wav',
+    f'{linein_lq_path}/lin-lq-2.13.27-base-5.wav'
+)
+
+linein_lq_21327_jbuf=(
+    f'{linein_lq_path}/linein_lq_21327_jbuf_1.wav',
+    f'{linein_lq_path}/linein_lq_21327_jbuf_2.wav',
+    f'{linein_lq_path}/linein_lq_21327_jbuf_3.wav',
+    f'{linein_lq_path}/linein_lq_21327_jbuf_4.wav',
+    f'{linein_lq_path}/linein_lq_21327_jbuf_5.wav'
 )
 
 def latency_stats(signal_path, responses_path_list):
@@ -70,3 +95,14 @@ print('\nCalculating LineIn to 4W latency[2 playback buffers]:')
 linein_lq_2b_avg, linein_lq_2b_c_std = latency_stats(loopback_signal, linein_lq_2b)
 print(f'LineIn - HS latency: {linein_lq_2b_avg:.2f} +/- {linein_lq_2b_c_std:.2}')
 
+print('\nCalculating Loopback latency[12.14.22 19:30 ARG]:')
+loopback_responses_141222_avg, loopback_responses_141222_std = latency_stats(loopback_signal, loopback_responses_141222)
+print(f'LineIn - HS latency: {loopback_responses_141222_avg:.2f} +/- {loopback_responses_141222_std:.2}')
+
+print('\nCalculating LineIn to 4W latency[2.13.27 Baseline]:')
+linein_lq_21327_avg, linein_lq_21327_std = latency_stats(loopback_signal, linein_lq_21327)
+print(f'LineIn - HS latency: {(linein_lq_21327_avg - loopback_responses_141222_avg):.2f} +/- {linein_lq_21327_std:.2}')
+
+print('\nCalculating LineIn to 4W latency[2.13.27 Jitter Buffer reduced]:')
+linein_lq_21327_jbuf_avg, llinein_lq_21327_jbuf_std = latency_stats(loopback_signal, linein_lq_21327_jbuf)
+print(f'LineIn - HS latency: {(linein_lq_21327_jbuf_avg - loopback_responses_141222_avg):.2f} +/- {llinein_lq_21327_jbuf_std:.2}')
